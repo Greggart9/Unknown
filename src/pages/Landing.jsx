7fd6b3c5f547628780810b6eccd2ce86cbccef2d
Greggart9 <retrievergolden498@gmail.com>
2024-10-30 06:16:48 +0100

    Refactor sidebar section layout and styles

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -124,7 +124,7 @@ return (
         <section className='flex justify-center items-center w-screen h-[520px] py-[96px] bg-gray-50'>
             <div className='bg-blue-900 flex justify-center w-[90%] h-[328px]'>
                 <div className='bg-blue-700 flex w-[95%] h-[100%] justify-between'>
-                    <img src='\image\img12.png' alt='Testimonial-image' className='w-[40%] h-[100%] rounded-2xl' />
+                    <img src='\image\img12.png' alt='Testimonial-image' className='w-[25%] h-[100%] rounded-2xl' />
 
                     <div className='bg-yellow-600 w-[50%] h-[80%]'>
                         <div className='bg-red-800 w-[100%] h-[70%] flex flex-col'>
@@ -145,33 +145,33 @@ return (
         </section>
 
         {/* Metric Section */}
-        <section className='flex justify-center items-center w-screen h-[432px] py-24 gap-16 bg-white'>
-            <div className='flex justify-center items-center w-[88.89%] h-[240px] py-8 gap-16'>
-                <div className='flex justify-between items-center w-full h-[240px] border-t border-b border-gray-200 py-16 '>
+        <section className='flex justify-center items-center w-screen h-[432px] py-24 gap-16 bg-blue-300'>
+            <div className='flex justify-center bg-slate-400 items-center w-[90%] h-[240px] py-8 gap-16'>
+                <div className='flex justify-between bg-white items-center w-[95%] h-[240px] border-t border-b border-gray-200 py-16 '>
                     <div>
                         <div className=' h-[112px] gap-5'>
-                            <p className='leading-[72px] text-center font-semibold text-[3.75rem] text-gray-900'>400+</p>
+                            <p className='leading-[72px] text-center font-semibold text-4xl lg:text-[3.75rem] text-gray-900'>400+</p>
                             <p className='h-[28px] text-lg font-medium leading-7 text-center'>Projects completed</p>
                         </div>
                     </div>
 
                     <div>
                         <div className='h-[112px] gap-5'>
-                            <p className=' h-[72px] leading-[72px] text-center font-semibold text-[3.75rem] text-gray-900'>600%</p>
+                            <p className=' h-[72px] leading-[72px] text-center font-semibold text-4xl lg:text-[3.75rem] text-gray-900'>600%</p>
                             <p className=' h-[28px] text-lg font-medium leading-7 text-center'>Return on investment</p>
                         </div>
                     </div>
 
                     <div>
                         <div className=' h-[112px] gap-5'>
-                            <p className=' h-[72px] leading-[72px] text-center font-semibold text-[3.75rem] text-gray-900'>10k</p>
+                            <p className=' h-[72px] leading-[72px] text-center font-semibold text-4xl lg:text-[3.75rem] text-gray-900'>10k</p>
                             <p className='h-[28px] text-lg font-medium leading-7 text-center'>Global downloads</p>
                         </div>
                     </div>
 
                     <div>
                         <div className='h-[112px] gap-5'>
-                            <p className=' h-[72px] leading-[72px] text-center font-semibold text-[3.75rem] text-gray-900'>200+</p>
+                            <p className=' h-[72px] leading-[72px] text-center font-semibold text-4xl lg:text-[3.75rem] text-gray-900'>200+</p>
                             <p className='h-[28px] text-lg font-medium leading-7 text-center'>5-star reviews</p>
                         </div>
                     </div>
@@ -180,78 +180,79 @@ return (
         </section>
 
         {/* Contact sections */}
-        <section className=' flex justify-center items-center w-screen h-[960px] pb-[96px] gap-16 bg-white'>
-            <div className=' flex w-[88.89%] h-[864px] px-8 gap-16'>
-                <img src='\image\img14.png' alt='Contact' className='w-[576px] h-[864px]' />
+        <section className=' flex justify-center w-screen h-[960px] pb-[96px] gap-16 bg-white'>
+            <div className='bg-red-600 flex justify-center w-[95%] h-[864px] px-8 gap-8'>
+                <img src='\image\img14.png' alt='Contact' className='w-[52%] h-[864px]' />
 
-                <div className=' flex justify-center items-center w-[576px] h-[864px]'>
-                    <div className='flex flex-col w-[480px] h-[712px] gap-12'>
+                <div className='bg-green-700 flex justify-center items-center w-[50%] h-[864px]'>
+                    <div className='flex flex-col w-[100%] h-[100%] gap-12'>
+
                         {/* Heading and Supporting text */}
-                        <div className='w-[480px] h-[94px] gap-5'>
-                            <p className='w-[480px] h-[44px] font-semibold text-4xl leading-11 text-gray-900'>Let's start your project</p>
-                            <p className='w-[480px] h-[44px] font-normal text-xl leading-7 text-gray-600'>Our friendly team would love to hear from you.</p>
+                        <div className='bg-red-500 w-[100%] flex flex-col  h-[15%]'>
+                            <p className='w-[100%] h-[45%] font-semibold text-3xl leading-7 text-gray-900'>Let's start your project</p>
+                            <p className='w-[100%] h-[45%] font-normal text-xl leading-5 text-gray-600'>Our friendly team would love to hear from you.</p>
                         </div>
 
                         {/* Form */}
-                        <form className='flex flex-col w-[480px] h-[570px] gap-8'>
+                        <form className='bg-green-400 flex flex-col w-[100%] h-[85%] gap-8'>
                              {/* Form field  */}
-                            <div className='w-[480px] h-[490px] gap-8'>
+                            <div className='w-[100%] h-[490px] justify-between'>
                                 {/* First and Last name */}
-                                <div className='flex gap-8 w-[480px] h-[70px] mb-5 '>
-                                    <div className='flex flex-col w-[224px] h-[70px] gap-[6px]'>
+                                <div className=' flex gap-11 w-[100%] h-[70px] mb-5'>
+                                    <div className='flex flex-col w-[45%] h-[70px] gap-[6px]'>
                                         <label htmlFor='firstName' className='text-gray-700 w-[71px] h-[20px] text-xs leading-5 font-medium'>First Name</label>
-                                        <div className='flex justify-center items-center w-[224px] h-[44px] gap-2 border border-gray-300 py-[10px] px-[14px] rounded'>
-                                            <input type='text' id='firstName' placeholder='First Name' className='w-[196px] h-[24px] gap-2 outline-none'  /></div>
+                                        <div className='flex justify-center items-center w-[100%] h-[44px] gap-2 border border-gray-300 py-[10px] px-[14px] rounded'>
+                                            <input type='text' id='firstName' placeholder='First Name' className='w-[100%] bg-transparent h-[24px] gap-2 outline-none'  /></div>
                                     </div>
 
-                                    <div className='flex flex-col w-[224px] h-[70px] gap-[6px]'>
+                                    <div className='flex flex-col w-[45%] h-[70px] gap-[6px]'>
                                         <label htmlFor='lastName' className='text-gray-700 w-[71px] h-[20px] text-xs leading-5 font-medium'>last Name</label>
-                                        <div className='flex justify-center items-center w-[224px] h-[44px] gap-2 border border-gray-300 py-[10px] px-[14px] rounded'>
-                                            <input type='text' id='lastName' placeholder='lastName' className='w-[196px] h-[24px] gap-2 outline-none'  /></div>
+                                        <div className='flex justify-center items-center w-[100%] h-[44px] gap-2 border border-gray-300 py-[10px] px-[14px] rounded'>
+                                            <input type='text' id='lastName' placeholder='lastName' className='w-[100%]  bg-transparent h-[24px] gap-2 outline-none'  /></div>
                                         </div>
                                 </div>
 
                                 {/* Email */}
-                                <div className='flex flex-col gap-[6px] w-[480px] h-[70px] mb-5 '>
+                                <div className='flex flex-col gap-[6px] w-[100%] h-[70px] mb-5 '>
                                     <label htmlFor='email' className='text-gray-700 w-[36px] h-[20px] text-xs leading-5 font-medium'>Email</label>
                                     
-                                    <div className='flex justify-center items-center w-[480px] h-[44px] gap-2 border border-gray-300 py-[10px] px-[14px] rounded'>
-                                    <input type='email' id='email' placeholder='greggart9@gmail.com' className='w-[452px] h-[24px] gap-2 outline-none p-2 rounded' />
+                                    <div className='flex justify-center items-center w-[100%] h-[44px] gap-2 border border-gray-300 py-[10px] px-[14px] rounded'>
+                                    <input type='email' id='email' placeholder='greggart9@gmail.com' className='w-[452px]  bg-transparent h-[24px] gap-2 outline-none p-2 rounded' />
                                     </div>
                                 </div>
                                 
                                 {/* Phone Number */}
-                                <div className='flex flex-col  gap-[6px] w-[480px] h-[70px] mb-5'>
+                                <div className='flex flex-col  gap-[6px] w-[100%] h-[70px] mb-5'>
                                     <label htmlFor='phone' className='text-gray-700 w-[98px] h-[20px] text-xs leading-5 font-medium'>Phone</label>
-                                    <div className='flex gap-2 w-[480px] h-[44px] border border-gray-300 p-2 rounded'>
-                                        <select id='countryCode' className='outline-none text-gray-900 text-lg font-normal leading-6'>
+                                    <div className='flex gap-2 w-[100%] h-[44px] border border-gray-300 p-2 rounded'>
+                                        <select id='countryCode' className='outline-none text-gray-900 text-lg  bg-transparent font-normal leading-6'>
                                             <option value='+1'>+1</option>
                                             <option value='+44'>+44</option>
                                             <option value='+91'>+91</option>
                                             {/* Add more country codes as needed */}
                                         </select>
-                                        <input type='text' id='phone' placeholder='+1 (555) 000-0000' className=' p-2 rounded flex-grow' />
+                                        <input type='text' id='phone' placeholder='+1 (555) 000-0000' className=' bg-transparent outline-none p-2 rounded flex-grow' />
                                     </div>
                                 </div>
 
                                 {/* Text Box */}
-                                <div className='w-[480px] h-[160px] gap-[6px] mb-5 '>
-                                <div className='flex flex-col w-[480px] h-[160px] gap-[6px]'>
+                                <div className='w-[100%] h-[160px] gap-[6px] mb-5 '>
+                                <div className='flex flex-col w-[100%] h-[160px] gap-[6px]'>
                                     <label htmlFor='message' className='text-gray-700 w-[61px] h-[20px] font-medium text-sm leading-5'>Message</label>
                                     <textarea id='message' rows='4' placeholder='Leave us a message...' className='border border-gray-300 p-2 rounded'></textarea>
                                 </div>
                                 </div>
 
                                 {/* Check box */}
-                                <div className='flex items-center w-[480px] h-[24px] gap-3 mb-5 '>
+                                <div className='flex items-center w-[100%] h-[24px] gap-3 mb-5'>
                                     <input type='checkbox' id='subscribe' className='w-[20px] h-[22px] pt-[2px]' />
                                     <label htmlFor='subscribe' className='text-gray-600 text-lg font-normal leading-6'>You agree to our friendly <span className='underline underline-offset-4'>privacy policy</span>.</label>
                                 </div>
                             </div>
 
                             {/* Actions */}
-                            <div className='w-[480px] h-[48px] gap-4'>
-                                <button className='w-[480px] h-[48px] bg-purple-600 text-white font-medium rounded-lg border border-purple-600 py-3 px-5'>Send message</button>
+                            <div className='w-[1005] h-[48px] gap-4'>
+                                <button className='w-[100%] h-[48px] bg-purple-600 text-white font-medium rounded-lg border border-purple-600 py-3 px-5'>Send message</button>
 
                             </div>
 
@@ -270,43 +271,43 @@ return (
         </section>
 
         {/* Footer */}
-        <footer className='w-screen h-[320px] pt-16 pb-12 gap-16 flex flex-col items-center justify-center'>
+        <footer className='bg-blue-600 w-screen h-[320px] pt-16 pb-12 gap-16 flex flex-col items-center justify-center'>
              {/* Container */}
-            <div className='w-[1280px] h-[88px] px-[32px] gap-12 flex justify-center'>
+            <div className='bg-red-500 w-[90%] h-[50%] flex justify-center'>
 
                 {/* Content */}
-                <div className=' flex w-5/6 h-[88px] gap-8'>
+                <div className='bg-orange-200 flex w-[93%] h-[88px] justify-between'>
 
                     {/* Logo and Links */}
-                    <div className='relative w-[824px] h-[88px] gap-8'>
+                    <div className='bg-green-500 flex flex-col justify-between w-[60%] h-[88px] gap-8'>
                     {/* Logo */}
-                        <div className='absolute top-0 flex text-center w-[142px] h-[32px]'>
+                        <div className=' flex text-center items-center w-[100%] h-[32px]'>
                             <img src="\image\Logo.png" alt="logo" className='w-[32px] h-[32px]'/>
-                            <h2 className='w-[100px] h-[32px] text-center text-lg font-semibold '>Untitled UI</h2>
+                            <h2 className='w-[100px] h-[32px] text-center lg:text-lg font-semibold '>Untitled UI</h2>
                         </div>
 
                         {/* footer links */}
-                        <div className=' absolute bottom-0 w-[824px] h-[24px] gap-8 flex'>
-                            <a href='/' className='text-gray-600 text-lg font-semibold leading-6'>Overview</a>
-                            <a href='/' className='text-gray-600 text-lg font-semibold leading-6'>Features</a>
-                            <a href='/' className='text-gray-600 text-lg font-semibold leading-6'>Pricing</a>
-                            <a href='/' className='text-gray-600 text-lg font-semibold leading-6'>Careers</a>
-                            <a href='/' className='text-gray-600 text-lg font-semibold leading-6'>Help</a>
-                            <a href='/' className='text-gray-600 text-lg font-semibold leading-6'>Privacy</a>
+                        <div className='w-[100%] h-[24px] justify-between flex'>
+                            <a href='/' className='text-gray-600  lg:text-xl font-semibold leading-6'>Overview</a>
+                            <a href='/' className='text-gray-600 lg:text-lg font-semibold leading-6'>Features</a>
+                            <a href='/' className='text-gray-600 lg:text-lg font-semibold leading-6'>Pricing</a>
+                            <a href='/' className='text-gray-600 lg:text-lg font-semibold leading-6'>Careers</a>
+                            <a href='/' className='text-gray-600 lg:text-lg font-semibold leading-6'>Help</a>
+                            <a href='/' className='text-gray-600 lg:text-lg font-semibold leading-6'>Privacy</a>
                         </div>
 
                     </div>
                      
                      {/* Newsletter */}
-                    <div  className='relative  w-[360px] h-[80px] gap-4'>
+                    <div  className='bg-blue-900 flex flex-col justify-between w-[40%] h-[100%] gap-4'>
                         <p className='h-[20px] text-gray-900 leading-5 font-semibold text-sm '>Stay up to date</p>
 
-                        <div className=' absolute bottom-0 flex h-[44px] gap-4'>
-                            <div className='w-[229px] border border-gray-300 rounded-lg flex items-center h-[44px] flex justify-center'>
-                                <input type='email' id='email' placeholder='Enter your email' className='w-[201px] h-[24px] outline-none bg-transparent'  />
+                        <div className=' flex w-[100%] h-[44px] gap-4'>
+                            <div className='w-[229px] border border-gray-300 rounded-lg flex items-center h-[44px] justify-center'>
+                                <input type='email' id='email' placeholder='Enter your email' className='w-[90%] h-[24px] outline-none bg-transparent'  />
                             </div>
                             
-                            <button className='border bg-purple-600 text-white border-purple-600 h-[44px] w-[115px] rounded-lg px-[18px] py-[10px]'>Subscribe</button>
+                            <button className='border bg-purple-600 text-white border-purple-600 h-[44px] text-sm w-[30%] rounded-lg px-[18px] py-[10px]'>Subscribe</button>
 
                         </div>
 
@@ -319,7 +320,7 @@ return (
             </div>
              
              {/* Container 2 */}
-            <div className=' w-[1280px] h-[56px] px-[32px] gap-8 flex justify-center'>
+            <div className='bg-slate-500 w-[90%] h-[40%] px-[32px] gap-8 flex justify-center'>
                 <div className=' flex w-5/6 h-[56px] border-t-2 border-gray-200 pt-8 gap-8 '>
                     <p className='text-gray-500 text-lg font-normal leading-6 w-[988px] h-[24px]'>© 2024 Untitled UI. All rights reserved.</p>
 
